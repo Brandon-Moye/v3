53218aa471c85877f3a268fbefaca99d11781589
Brandon Moye <brandon.m.moye@gmail.com>
2023-11-15 19:39:17 -0600

    keydown event

--- whosThatApi/index.js
+++ whosThatApi/index.js
@@ -10,35 +10,35 @@ function searchForPokemon() {
             'Content-Type': 'application/json'
         }
     })
-    
-    .then(res => {
-        if (!res.ok) {
-            throw new Error('Network response was not okay')
-        }
-        return res.json()
-    })
-    .then(data => {
-        document.getElementById('pokemonImage').src = data['Pokemon Details']['pokemonImage']
-        function setTextContentById(id, value) {
-            const element = document.getElementById(id)
-            if (element && element !== 'pokemonImage') {
-                element.textContent = value;
+
+        .then(res => {
+            if (!res.ok) {
+                throw new Error('Network response was not okay')
             }
-        }
-    
-        Object.keys(data['Pokemon Details']).forEach(key => {
-            const value = data['Pokemon Details'][key]
-            return setTextContentById(key, value)
-            
+            return res.json()
+        })
+        .then(data => {
+            document.getElementById('pokemonImage').src = data['Pokemon Details']['pokemonImage']
+            function setTextContentById(id, value) {
+                const element = document.getElementById(id)
+                if (element && element !== 'pokemonImage') {
+                    element.textContent = value;
+                }
+            }
+
+            Object.keys(data['Pokemon Details']).forEach(key => {
+                const value = data['Pokemon Details'][key]
+                return setTextContentById(key, value)
+
+            })
+
+            console.log(data)
+
+        })
+        .catch(error => {
+            return (error)
         })
 
-        console.log(data)
-        
-    })
-    .catch(error => {
-        return (error)
-    })
-    
     pokemonInput.value = '';
 }
 
@@ -46,7 +46,7 @@ searchForPokemon()
 
 searchButton.addEventListener('click', searchForPokemon)
 pokemonInput.addEventListener('keydown', e => {
-    if(e.key === 'Enter') {
+    if (e.key === 'Enter') {
         searchForPokemon()
     }
 })
